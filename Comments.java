7d3cb84af70f60a49e5e15ed953c03f0f4afcaa2
Aman Sharma <mannu.poski10@gmail.com>
2021-12-08 13:27:22 +0100

    Comments

---
+++ Comments.java
@@ -0,0 +1,4 @@
+public class Comments {
+    // I am an empty class.
+    // However, I will not state this concisely and make this comment super long so that you have a hard time scrolling on GitHub. You will realise that reading this comment is of no use to you, but you still are not averting your eyes. If you reached the end, please get a life.
+}
