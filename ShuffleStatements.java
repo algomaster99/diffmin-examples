5d6eae0d19270b515eb9c81fd3a8712be78121a5
Aman Sharma <mannu.poski10@gmail.com>
2021-12-29 16:52:14 +0100

    Statements inside if-elseif-else block

---
+++ ShuffleStatements.java
@@ -0,0 +1,13 @@
+class ShuffleStatements {
+    int a = 1;
+    void checkForSign() {
+        if (a > 0) {
+            System.out.println("y");
+        } else if (a < 0) {
+            System.out.println("z");
+        } else {
+            System.out.println("x");
+        }
+    }
+}
+
